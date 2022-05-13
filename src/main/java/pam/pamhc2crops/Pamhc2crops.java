037dfd0d1a6f06664a24ac54282435f387cfaa77
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-05-13 19:50:04 -0400

    Move common code out of the proxies.

--- src/main/java/pam/pamhc2crops/Pamhc2crops.java
+++ src/main/java/pam/pamhc2crops/Pamhc2crops.java
@@ -6,14 +6,27 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 
+import pam.pamhc2crops.config.Config;
+import pam.pamhc2crops.events.EventSetup;
+import pam.pamhc2crops.init.BlockRegistry;
+import pam.pamhc2crops.init.CompostRegistry;
 import pam.pamhc2crops.init.ItemRegistry;
+import pam.pamhc2crops.init.WorldGenRegistry;
 import pam.pamhc2crops.worldgen.GardenGeneration;
 
 @Mod("pamhc2crops")
@@ -30,10 +43,29 @@ public class Pamhc2crops {
 	};
 
 	public Pamhc2crops() {
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.CONFIG, "pamhc2crops.toml");
+		Config.loadConfig(Config.CONFIG, FMLPaths.CONFIGDIR.get().resolve("pamhc2crops.toml").toString());
+
+		final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modBus.addListener(Pamhc2crops::commonSetup);
+		modBus.addGenericListener(Block.class, BlockRegistry::registerAll);
+		modBus.addGenericListener(Item.class, ItemRegistry::registerAll);
+
+		modBus.addGenericListener(Feature.class, WorldGenRegistry::registerFeatures);
+
 		MinecraftForge.EVENT_BUS.addListener(GardenGeneration::addFeaturesToBiomes);
 
-		// Cleaned up proxies and swapped depreciated method for safeRunForDist
 		DistExecutor.safeRunForDist(() -> SideProxy.Client::new, () -> SideProxy.Server::new);
+	}
+
+	private static void commonSetup(FMLCommonSetupEvent event) {
+		EventSetup.setupEvents();
+
+		event.enqueueWork(() -> {
+			WorldGenRegistry.registerConfiguredFeatures();
+			WorldGenRegistry.registerPlacedFeatures();
+			CompostRegistry.register();
+		});
 	}
 
 	@Nonnull
--- src/main/java/pam/pamhc2crops/SideProxy.java
+++ src/main/java/pam/pamhc2crops/SideProxy.java
@@ -1,42 +1,12 @@
 package pam.pamhc2crops;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
 
-import pam.pamhc2crops.config.Config;
-import pam.pamhc2crops.events.EventSetup;
-import pam.pamhc2crops.init.BlockRegistry;
-import pam.pamhc2crops.init.CompostRegistry;
-import pam.pamhc2crops.init.ItemRegistry;
 import pam.pamhc2crops.init.ModRenderers;
-import pam.pamhc2crops.worldgen.GardenGeneration;
 
 public class SideProxy {
-	SideProxy() {
-		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.CONFIG, "pamhc2crops.toml");
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(SideProxy::commonSetup);
-		FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Block.class, BlockRegistry::registerAll);
-
-		FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Item.class, ItemRegistry::registerAll);
-
-		Config.loadConfig(Config.CONFIG, FMLPaths.CONFIGDIR.get().resolve("pamhc2crops.toml").toString());
-	}
-
-
-	private static void commonSetup(FMLCommonSetupEvent event) {
-		EventSetup.setupEvents();
-		CompostRegistry.register();
-	}
+	SideProxy() {}
 
 	static class Client extends SideProxy {
 		Client() {
