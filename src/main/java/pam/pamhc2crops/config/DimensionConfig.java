c873ad8d0788f32e6959084a0efd1cf819a06924
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-05-17 11:21:54 -0400

    Comment out dimension stuff as that hasn't been hooked back up yet.

--- src/main/java/pam/pamhc2crops/config/DimensionConfig.java
+++ src/main/java/pam/pamhc2crops/config/DimensionConfig.java
@@ -8,14 +8,14 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class DimensionConfig {
 
-	public static ForgeConfigSpec.ConfigValue<List<? extends String>> whitelist;
-	public static ForgeConfigSpec.ConfigValue<List<? extends String>> blacklist;
+	//public static ForgeConfigSpec.ConfigValue<List<? extends String>> whitelist;
+	//public static ForgeConfigSpec.ConfigValue<List<? extends String>> blacklist;
 
 	public static void init(ForgeConfigSpec.Builder config) {
-		whitelist = config.comment("Enter a dimension id to whitelist feature generation").defineList("white_dim",
+		/*whitelist = config.comment("Enter a dimension id to whitelist feature generation").defineList("white_dim",
 				Arrays.asList("minecraft:overworld"), val -> val instanceof String string && ResourceLocation.tryParse(string) != null);
 
 		blacklist = config.comment("Enter a dimension id to blacklist feature generation").defineList("black_dim",
-				Arrays.asList("minecraft:the_end", "minecraft:the_nether"), val -> val instanceof String string && ResourceLocation.tryParse(string) != null);
+				Arrays.asList("minecraft:the_end", "minecraft:the_nether"), val -> val instanceof String string && ResourceLocation.tryParse(string) != null);*/
 	}
 }
